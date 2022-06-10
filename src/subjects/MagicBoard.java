b119c5d6d277ab8826077a69569c32cf6510ffda
rubylele <smthmyfam@gmail.com>
2022-06-10 19:39:12 +0300

    creating GET command classes

--- src/subjects/MagicBoard.java
+++ src/subjects/MagicBoard.java
@@ -12,4 +12,51 @@ public class MagicBoard implements Observable {
     public MagicBoard() {
         dwarfsInService = new ArrayList<>();
     }
+
+    @Override
+    public void subscribe(Observer observer) {
+        if(observer != null) {
+            dwarfsInService.add(observer);
+            observer.setMagicBoard(this);
+        } else {
+            throw new IllegalArgumentException("The dwarf cannot be null!");
+        }
+    }
+
+    @Override
+    public void unsubscribe(Observer observer) {
+        if(observer != null) {
+            dwarfsInService.remove(observer);
+            observer.removeMagicBoard();
+        } else {
+            throw new IllegalArgumentException("The dwarf cannot be null!");
+        }
+    }
+
+    @Override
+    public void notifyObservers() {
+        for(Observer dwarf : dwarfsInService) {
+            dwarf.update();
+        }
+    }
+
+    @Override
+    public String getToy() {
+        return requestedToy;
+    }
+
+    public void requestToyFromDwarfs(String requestedToyFromSanta) {
+        if(requestedToyFromSanta == null || requestedToyFromSanta.isBlank()) {
+            throw new IllegalArgumentException("The requested toy cannot be null or blank!");
+        }
+        requestedToy = requestedToyFromSanta;
+        System.out.printf("%s is written on the board! \n", requestedToyFromSanta);
+        notifyObservers();
+        System.out.println("---------------------------------------------------");
+    }
+
+	public void requestToyFromDwarfs(String requestedToyFromSanta) {
+		// TODO Auto-generated method stub
+		
+	}
 }
